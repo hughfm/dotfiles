63d2b71b037cde2325dbd8e0404bdb14c46d6460
Hugh Middleton <hughm@canva.com>
2021-09-28 16:32:32 +1000

    lsp fix

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -12,28 +12,6 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
--- function PrintDiagnostics(opts, bufnr, line_nr, client_id)
---   opts = opts or {}
--- 
---   bufnr = bufnr or 0
---   line_nr = line_nr or (vim.api.nvim_win_get_cursor(0)[1] - 1)
--- 
---   local line_diagnostics = vim.lsp.diagnostic.get_line_diagnostics(bufnr, line_nr, opts, client_id)
---   if vim.tbl_isempty(line_diagnostics) then return end
--- 
---   local diagnostic_message = ""
---   for i, diagnostic in ipairs(line_diagnostics) do
---     diagnostic_message = diagnostic_message .. string.format("%d: %s", i, diagnostic.message or "")
---     print(diagnostic_message)
---     if i ~= #line_diagnostics then
---       diagnostic_message = diagnostic_message .. "\n"
---     end
---   end
---   vim.api.nvim_echo({{diagnostic_message, "Normal"}}, false, {})
--- end
--- 
--- vim.cmd [[ autocmd CursorHold * lua PrintDiagnostics() ]]
-
 local M = {}
 
 M.icons = {
@@ -102,37 +80,12 @@ local on_attach = function(client, bufnr)
 
   vim.lsp.handlers["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, { border = "double" })
   vim.lsp.handlers["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.hover, { border = "double" })
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = function(_, _, params, client_id, _)
-    local config = { -- your config
-      underline = true,
-      virtual_text = {
-        prefix = "■ ",
-        spacing = 4,
-      },
-      signs = true,
-      update_in_insert = false,
-    }
-    local uri = params.uri
-    local bufnr = vim.uri_to_bufnr(uri)
-
-    if not bufnr then
-      return
-    end
-
-    local diagnostics = params.diagnostics
-
-    for i, v in ipairs(diagnostics) do
-      diagnostics[i].message = string.format("%s: %s", v.source, v.message)
-    end
-
-    vim.lsp.diagnostic.save(diagnostics, bufnr, client_id)
-
-    if not vim.api.nvim_buf_is_loaded(bufnr) then
-      return
-    end
-
-    vim.lsp.diagnostic.display(diagnostics, bufnr, client_id, config)
-  end
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = {
+      prefix = "■ ",
+      spacing = 4,
+    },
+  })
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
