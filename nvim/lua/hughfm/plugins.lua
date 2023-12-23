458583a89237811f57c383d63b5ce48c1c2cdbd9
Hugh Middleton <hughm@canva.com>
2023-12-23 14:10:54 +1100

    remove eslint file

--- nvim/lua/hughfm/plugins.lua
+++ nvim/lua/hughfm/plugins.lua
@@ -40,7 +40,6 @@ return require('packer').startup {
     use {
       'neovim/nvim-lspconfig',
       config = function()
-        require('hughfm.eslint')
         require('hughfm.lsp')
       end,
       requires = {
