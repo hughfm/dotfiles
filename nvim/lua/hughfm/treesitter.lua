6a02c1f300b6d41dc5ef3477ec7790107ac60249
Hugh Middleton <hughm@canva.com>
2023-12-30 22:21:04 +1100

    vimdoc treesitter

--- nvim/lua/hughfm/treesitter.lua
+++ nvim/lua/hughfm/treesitter.lua
@@ -1,7 +1,7 @@
 -- target: ~/.config/nvim/lua/hughfm/treesitter.lua
 
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "javascript", "typescript", "tsx", "query", "lua", "json", "jsonc", "css", "graphql", "php", "proto", "java", "prisma" },
+  ensure_installed = { "javascript", "typescript", "tsx", "query", "lua", "json", "jsonc", "css", "graphql", "php", "proto", "java", "prisma", "vimdoc" },
   highlight = { enable = true },
   incremental_selection = { enable = true },
   textobjects = {
