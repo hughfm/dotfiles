79665682bc227c853cdb7a45773767f544b44d14
Hugh Middleton <hughm@canva.com>
2024-01-24 11:08:48 +1100

    lualine separators

--- nvim/lua/hughfm/lualine.lua
+++ nvim/lua/hughfm/lualine.lua
@@ -1,5 +1,9 @@
 require('lualine').setup {
-  options = { theme = 'catppuccin-frappe' },
+  options = {
+    theme = 'catppuccin-frappe',
+    component_separators = '',
+    section_separators = '',
+  },
   sections = {
     lualine_c = { 'filename', 'nvim_treesitter#statusline' },
   }
